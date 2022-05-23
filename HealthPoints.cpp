8cf713c076a85573f95634e4ffad9f7eed3f1983
mohamad-tay <mohamad-tay@campus.technion.ac.il>
2022-05-23 12:26:22 +0300

    adding healthpoints (cpp and h)

---
+++ HealthPoints.cpp
@@ -0,0 +1,115 @@
+#include "HealthPoints.h"
+#include <iostream>
+
+
+const int MAX_HEALTH_DEFAULT = 100;
+
+
+HealthPoints::HealthPoints(int maxHp)
+{
+    if(maxHp<=0)
+    {
+        throw InvalidArgument(maxHp);                        // b3rfsh aethafe 7agy
+    }
+    else
+    {
+        m_hp = maxHp;
+        m_maxHp = maxHp;
+    }
+}
+
+
+HealthPoints& HealthPoints::operator+=(const int extraHealth)
+{
+    if((m_hp+extraHealth)>m_maxHp)
+    {
+        m_hp = m_maxHp;
+    }
+    else if((m_hp+extraHealth)<0)
+    {
+        m_hp = 0;
+    }
+    else
+    {
+        m_hp += extraHealth;
+    }
+    return *this;
+}
+
+
+HealthPoints HealthPoints::operator+(const int extraHealth) const
+{
+    HealthPoints result(*this);
+    return(result += extraHealth);
+}
+
+HealthPoints& HealthPoints::operator-=(const int extraHealth)
+{
+    int temp = (-1)*(extraHealth);
+    return *this += temp;
+}
+
+
+HealthPoints HealthPoints::operator-(const int extraHealth) const
+{
+    HealthPoints result(*this);
+    return(result -= extraHealth);
+}
+
+//------------------------------------------------------------------------------------------------
+
+HealthPoints HealthPoints::operator+(const int extraHealth, const HealthPoints& health) const
+{
+    HealthPoints result(health);
+    return(result += extraHealth);
+ }
+
+
+ HealthPoints HealthPoints::operator-(const int extraHealth, const HealthPoints& health) const
+ {
+    HealthPoints result(health);
+    health.m_hp = (-1)*health.m_hp
+    return result += extraHealth;
+}
+
+ //-------------------------------------------------------------------------------------------------
+
+bool HealthPoints::operator==(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    return(healthPoints1.m_hp == healthPoints2.m_hp)
+}
+
+bool HealthPoints::operator!=(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    return(!( healthPoints1 ==  healthPoints2))
+}
+
+bool HealthPoints::operator<(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    if( healthPoints1.m_hp < healthPoints2.m_hp)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool HealthPoints::operator>(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    if(!(healthPoints1 < healthPoints2) && (healthPoints1 !=  healthPoints2) )
+}
+
+bool HealthPoints::operator<=(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    return(!(healthPoints1 > healthPoints2))
+}
+
+bool HealthPoints::operator>=(const HealthPoints healthPoints1, const HealthPoints healthPoints1)
+{
+    return(!(healthPoints1 < healthPoints2));
+}
+
+
+std::ostream& HealthPoints::operator<<(std::ostream os , const HealthPoints healthPoints)
+{
+    os << m_hp << "(" << m_maxHp << ")"
+}
---
+++ HealthPoints.h
@@ -0,0 +1,43 @@
+#ifndef EX3_HEALTHPOINTS_H
+#define EX3_HEALTHPOINTS_H
+
+
+class HealthPoints
+{
+    private:
+    int m_hp;
+    int m_maxHp;
+
+    public:
+    HealthPoints(int maxHp=100);
+    HealthPoints(HealthPoints health&)=defult;
+    ~HealthPoints()=defult;    
+    HealthPoints& operator+=(const int extraHealth); 
+    HealthPoints operator+(const int extraHealth) const; 
+    HealthPoints& operator-=(const int extraHealth) const;
+    HealthPoints operator-(const int extraHealth); 
+
+    HealthPoints operator+(const int extraHealth, const HealthPoints& health) const;           //fsh 7agy refrence lano int
+    HealthPoints operator-(const int extraHealth, const HealthPoints& health) const;           // rkm-h?? 
+
+    bool operator==(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+    bool operator!=(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+    bool operator<(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+    bool operator>(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+    bool operator<=(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+    bool operator>=(const HealthPoints healthPoints1, const HealthPoints healthPoints1);
+
+    friend std::ostream& operator<<(std::ostream os , const HealthPoints healthPoints);
+
+
+
+    class InvalidArgument{ int m_Invalid;};
+    
+};
+
+
+
+
+
+
+#endif //EX3_HEALTHPOINTS_H
